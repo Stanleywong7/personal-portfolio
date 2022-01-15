883d6a12acf9e1ced0fa07d389f427b9a96a52e9
Stanley Wong <31183863+Stanleywong7@users.noreply.github.com>
2022-01-15 00:54:05 -0500

    Minor Content Updates

--- src/components/Header/HeaderStyles.js
+++ src/components/Header/HeaderStyles.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   grid-template-columns: repeat(5, 1fr);
   grid-template-rows: 1fr;
   grid-column-gap: 2rem;
-  padding: 1rem;
+  padding: 2rem;
   padding-top: 2rem;
 
   @media ${(props) => props.theme.breakpoints.sm} {
@@ -42,7 +42,7 @@ export const Div2 = styled.div`
 `;
 
 export const Div3 = styled.div`
-  grid-area: 1 / 4 / 5 / 6;
+  grid-area: 2 / 4 / 5 / 6;
   display: flex;
   justify-content: space-around;
   align-items: center;
